31c50cadf8f33f4ce57b98d22683593c6a6c5b1b
SfsWasHere <78618911+SfsWasHere@users.noreply.github.com>
2021-04-17 13:48:23 +0100

    main.py
    report bot

---
+++ main.py
@@ -0,0 +1,34 @@
+#SFS MADE THIS 
+import requests
+import colorama
+import threading
+import os
+import ctypes
+from colorama import Fore, Style
+from threading import Thread
+from sys import stdout
+from requests import Session
+from time import strftime, gmtime
+
+sent = 0
+session = Session()
+b = Style.BRIGHT
+os = os.system
+os('cls')
+
+ctypes.windll.kernel32.SetConsoleTitleW(f"[REPORT BOT] By Sfs | Skid = my son 4L")
+
+print(f"""
+
+{b+Fore.BLUE}  _____                       
+ |  __ \                     | |    
+ | |__) |___ _ __   ___  _ __| |_    
+ |  _  // _ \ '_ \ / _ \| '__| __|   
+ | | \ \  __/ |_) | (_) | |  | |_    
+ |_|  \_\___| .__/ \___/|_|   \__|   
+            | |                                   
+            |_|                                   
+
+PS: Must Have Admin On The Server ;)
+
+{b+Fore.BLUE} > {Fore.RESET}Creator: sfs#9000
